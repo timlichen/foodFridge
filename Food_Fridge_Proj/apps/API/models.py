726495b65a974dc048e6940f4a6a37efce4b183a
NghiaVNguyen <nvnguyen90@gmail.com>
2016-06-27 13:24:07 -0700

    changes made

--- Food_Fridge_Proj/apps/API/models.py
+++ Food_Fridge_Proj/apps/API/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+
 import bcrypt
 import re
 from django.db import models
--- Food_Fridge_Proj/apps/API/urls.py
+++ Food_Fridge_Proj/apps/API/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index),
-	url(r'^login$', views.login),
-	url(r'^register$', views.register),
+
 ]
